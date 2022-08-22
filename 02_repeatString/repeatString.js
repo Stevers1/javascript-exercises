97d24803dfb454d63374f4dbcaf4b43ea9a70301
Steve <uberkootra@gmail.com>
2022-08-21 20:30:59 -0500

    Finished 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+  let stringR=string;
+
+  for(let i = 1;i<num; i++){
+    stringR+=string;
+  }
+
+  if(num <0){
+    return "ERROR"
+  }
+  else if(num==0) {
+    return ""
+  }
+
+  return stringR;
 };
 
+console.log(repeatString);
 // Do not edit below this line
 module.exports = repeatString;
