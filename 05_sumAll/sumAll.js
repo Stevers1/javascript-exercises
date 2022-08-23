d8fbd62cbf7fe4f444089b093dd5e75159cff924
Steve <uberkootra@gmail.com>
2022-08-23 18:33:52 -0500

    Cpmpleteted 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+  let sum = 0;
+  let args = Array.from(arguments)
+  args = args.sort(function(a, b){return a-b})
+
+  for(let x = 0; x < args.length; x++){
+    if(args[x] < 0) {
+      return "ERROR"
+    }
+
+    if(typeof args[x] !== 'number') {
+      return "ERROR"
+    }
+  }
+
+  for(let i = args[0]; i<=args[1]; i++){
+
+    sum+=i
+
+  }
+
+  return sum;
+
 
 };
 
