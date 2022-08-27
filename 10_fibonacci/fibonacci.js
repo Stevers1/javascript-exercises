8a84f39db1d630b8601e6b6a01124aa77c55f11a
Steve <uberkootra@gmail.com>
2022-08-27 12:56:10 -0500

    Completed 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+
+  let arr = [1];
+  let sum = 0;
+
+  if(x < 0) {
+    return "OOPS";
+  }
+
+  for(let i = 0; i < x; i++) {
+
+
+    if(i == 0){
+
+      sum = arr[0];
+      arr.push(sum)
+      sum = 0;
+      continue;
+    }
+
+    sum = arr[i]+arr[i-1]
+    arr.push(sum)
+    sum = 0;
+
+  }
+
+  return arr[arr.length-2]
 
 };
 
